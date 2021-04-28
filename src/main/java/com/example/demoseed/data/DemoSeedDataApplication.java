408e3f83b62a46d98a65667a2d13f76cbfe1c2c0
jonyboi21 <jonyboi21@github.com>
2021-04-27 21:31:34 -0400

    first commit

---
+++ src/main/java/com/example/demoseed/data/DemoSeedDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.demoseed.data;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSeedDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSeedDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demoseed/data/DemoSeedDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoseed.data;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSeedDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
